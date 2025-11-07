a782c2041b8a6dee1f891c36fc2f42a0a8890ff7
AlexWeatherwax <77688235+AlexWeatherwax@users.noreply.github.com>
2025-11-07 17:07:44 +0300

    Update settings.py

--- mobius_clinica/settings.py
+++ mobius_clinica/settings.py
@@ -136,11 +136,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
