91830b3c20fc6268640f16fac2b0f7775f59ec32
AlexWeatherwax <77688235+AlexWeatherwax@users.noreply.github.com>
2025-11-07 16:40:04 +0300

    Update settings.py

--- mobius_clinica/settings.py
+++ mobius_clinica/settings.py
@@ -141,6 +141,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
